e9b710aaf4f4943bebd23bd807ca5f92a510ac7e
Clacky <develop@clacky.ai>
2025-12-14 08:23:19 -0500

    Initial commit: Rails project setup
    Co-authored-by: SengMitnick <sengmitnick@163.com>

---
+++ app/javascript/controllers/index.ts
@@ -0,0 +1,15 @@
+import { Application } from "@hotwired/stimulus"
+
+import ThemeController from "./theme_controller"
+import DropdownController from "./dropdown_controller"
+import SdkIntegrationController from "./sdk_integration_controller"
+import ClipboardController from "./clipboard_controller"
+
+const application = Application.start()
+
+application.register("theme", ThemeController)
+application.register("dropdown", DropdownController)
+application.register("sdk-integration", SdkIntegrationController)
+application.register("clipboard", ClipboardController)
+
+window.Stimulus = application
