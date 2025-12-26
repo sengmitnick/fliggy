c561eaa783acd1b812f220bdfef30928ccb8f92c
Clacky <develop@clacky.ai>
2025-12-26 04:27:10 -0500

    feat: remember user's last selected destination
    - Store last destination in session
    - Display remembered destination on homepage
    - Default to Shenzhen if no previous selection
    
    Co-authored-by: SengMitnick <sengmitnick@163.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   include HomeDemoConcern
 
   def index
+    # 获取用户上次选择的城市，如果没有则默认为深圳
+    if session[:last_destination_slug].present?
+      @current_destination = Destination.friendly.find(session[:last_destination_slug])
+    else
+      @current_destination = Destination.friendly.find('shen-zhen')
+    end
   end
 end
