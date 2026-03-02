b1df7fb94723c333a948d4b2ebfbf3470b1761fa
Clacky <develop@clacky.ai>
2026-03-01 21:51:21 -0500

    fix: update schema version to 2026_02_28_101458 and fix v106 validator weight distribution

--- lib/data_pack_validator.rb
+++ lib/data_pack_validator.rb
@@ -24,7 +24,7 @@ class DataPackValidator
   
   # Schema version constant - updated by AI when schema changes
   # This version should match db/schema.rb ActiveRecord::Schema.define(version: ...)
-  VALIDATED_SCHEMA_VERSION = '2026_02_05_115551'
+  VALIDATED_SCHEMA_VERSION = '2026_02_28_101458'
   
   def initialize
     @errors = []
